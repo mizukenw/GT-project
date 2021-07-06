1efbf629ed8a8c173d9104fc4d8d0eae60584368
mizukenw <kentamizu75@gmail.com>
2021-07-06 22:03:43 +0900

    コメント機能追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,10 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :movies, dependent: :destroy
   has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
+  has_many :comments, dependent: :destroy
   accepts_nested_attributes_for :movies
 
-  def liked_by(user)
+  def liked_by(current_user)
     Like.find_by(user_id: user.id, post_id: id)
   end 
 end
